0046c95b10af353645919973eb06fb9d2f63f095
chucey <100479500+chucey@users.noreply.github.com>
2024-03-19 10:21:52 -0400

    Update Views.sql

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,9 +1,18 @@
 --Create different views for the web app
 
---Create a a view for assigned Tasks
-CREATE VIEW All_Assigned_Tasks (assigned_tasks, USERS) AS
-    SELECT (u.first_name || ' ' || u.Last_Name) as Assigned_To,
-            ast.Task_Name, ast.Description, Date(ast.Duedate)
-    FROM USERS u Join Assigned_Tasks ast
-    ON u.userID = ast.Assigned_To
+Drop VIEW All_assigned_tasks;
 
+Create VIEW All_assigned_tasks AS
+
+    SELECT (u.first_name || ' ' || u.Last_Name) as Assigned_By,
+    ast.task_name,
+    e.event_name,
+    trunc(ast.assign_date_time) as assigned_date,
+    (u2.first_name || ' ' || u2.Last_Name) as Assigned_To,
+    trunc(ast.duedate) as due_date
+    FROM assigned_tasks ast Inner JOIN users u on u.userID = ast.userID 
+    Left JOIN users u2 on u2.userID = ast.Assigned_to
+    left JOIN satisfies s On s.taskid = ast.taskid
+    left JOIN event_features ef on ef.featureid = s.featureid
+    left JOIN associated_with aw on aw.featureid = ef.featureid
+    left JOIN events e on e.eventid = aw.eventid;
