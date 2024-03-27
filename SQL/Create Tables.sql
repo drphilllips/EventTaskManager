2621edacf944e0cdc6b53543200b1098c3e1cbb4
chucey <100479500+chucey@users.noreply.github.com>
2024-03-27 10:39:38 -0400

    Update Create Tables.sql

--- SQL/Create Tables.sql
+++ SQL/Create Tables.sql
@@ -1,45 +1,45 @@
 --DROP Tables
 
 --Foreign Key
-DROP TABLE Is_Present_at;
-DROP TABLE Administrates;
-DROP TABLE Hosts;
-DROP TABLE Event_Hosts;
-DROP TABLE Admins;
-DROP TABLE Satisfies;
+DROP TABLE if exists Is_Present_at ;
+DROP TABLE if exists Administrates ;
+DROP TABLE if exists Hosts ;
+DROP TABLE if exists Event_Hosts ;
+DROP TABLE if exists Admins ;
+DROP TABLE if exists Satisfies ;
 --DROP TABLE Subtask_of;
-DROP TABLE Is_Contacted;
+DROP TABLE if exists Is_Contacted ;
 --DROP TABLE Assigned_Tasks;
 --DROP TABLE Completed_Tasks;
-DROP TABLE Tasks;
-DROP TABLE Business_Contacts;
-DROP TABLE Associated_With;
-DROP TABLE Event_Features;
-DROP TABLE EVENTS;
-DROP TABLE Locations;
-DROP TABLE Vendor;
-DROP TABLE Caterer;
+DROP TABLE if exists Tasks ;
+DROP TABLE if exists Business_Contacts;
+DROP TABLE if exists Associated_With ;
+DROP TABLE if exists Event_Features ;
+DROP TABLE if exists EVENTS ;
+DROP TABLE if exists Locations ;
+DROP TABLE if exists Vendor ;
+DROP TABLE if exists Caterer ;
 --DROP TABLE Tasks;
 
 --No Foriegn Key
-DROP TABLE Company;
-DROP TABLE Feature_Inventory;
-DROP TABLE Event_Types;
-DROP TABLE Location_types;
-DROP TABLE USERS;
-DROP TABLE Task_Type;
+DROP TABLE if exists Company ;
+DROP TABLE if exists Feature_Inventory ;
+DROP TABLE if exists Event_Types ;
+DROP TABLE if exists Location_types ;
+DROP TABLE if exists USERS ;
+DROP TABLE if exists Task_Type ;
 
 -- Sequences used to generate automatic primary keys (in oracle)
-DROP SEQUENCE Task_Type_seq;
-DROP SEQUENCE Task_seq;
-DROP SEQUENCE Company_seq;
-DROP SEQUENCE Business_Contacts_seq;
-DROP SEQUENCE Feature_Inventory_seq;
-DROP SEQUENCE Event_Feature_seq;
-DROP SEQUENCE Event_Type_seq;
-DROP SEQUENCE Location_Type_seq;
-DROP SEQUENCE Location_seq;
-DROP SEQUENCE Event_seq;
+DROP SEQUENCE if exists Task_Type_seq ;
+DROP SEQUENCE if exists Task_seq ;
+DROP SEQUENCE if exists Company_seq ;
+DROP SEQUENCE if exists Business_Contacts_seq ;
+DROP SEQUENCE if exists Feature_Inventory_seq ;
+DROP SEQUENCE if exists Event_Feature_seq ;
+DROP SEQUENCE if exists Event_Type_seq ;
+DROP SEQUENCE if exists Location_Type_seq ;
+DROP SEQUENCE if exists Location_seq ;
+DROP SEQUENCE if exists Event_seq ;
 
 --Schemsa, Sequences and Tables
 
@@ -57,13 +57,13 @@ CREATE TABLE USERS (
     Email VARCHAR(50) UNIQUE
 );
 
--- "Event_Hosts(UserID: String, Student_ID_number: string)
+-- "Event_Hosts(UserID: String, Student_ID_integer: string)
 -- Primary Key: UserID
--- Candidate Key: Student_ID_number
+-- Candidate Key: Student_ID_integer
 -- Foriegn Key: UserID"
 CREATE TABLE Event_Hosts (
     UserID VARCHAR(20) PRIMARY KEY,
-    Student_ID_number CHAR(9) UNIQUE,
+    Student_ID_integer CHAR(9) UNIQUE,
     FOREIGN KEY (UserID) REFERENCES USERS (UserID) 
 );
 
@@ -77,7 +77,7 @@ CREATE TABLE Admins (
     FOREIGN KEY (UserID) REFERENCES USERS (UserID) 
 );
 
--- "Task_Type(TypeID: number, Name: String)
+-- "Task_Type(TypeID: integer, Name: String)
 -- Primary Key: TaskID
 -- No Candidate Key
 -- No Foreign Key"
@@ -86,16 +86,16 @@ CREATE SEQUENCE Task_Type_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Task_Type (
-    TypeID NUMBER PRIMARY KEY,
+    TypeID integer PRIMARY KEY,
     Type_Name VARCHAR(30)
 );
 
 
--- "Tasks(TaskID: number, Name: String, Duedate: Date, Notes: String, 
--- Description: String, TypeID: number)
+-- "Tasks(TaskID: integer, Name: String, Duedate: Date, Notes: String, 
+-- Description: String, TypeID: integer)
 -- Primary Key: TaskID
 -- No Cnadidate Key
 -- Foriegn Key: TypeID"
@@ -103,33 +103,33 @@ CREATE SEQUENCE Task_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 -- CREATE TABLE Tasks (
---     TaskID NUMBER PRIMARY KEY,
+--     TaskID integer PRIMARY KEY,
 --     Task_Name VARCHAR(35),
 --     Duedate TIMESTAMP,
 --     Notes VARCHAR(250),
 --     Description VARCHAR(100),
---     TypeID NUMBER,
+--     TypeID integer,
 --     FOREIGN KEY (TypeID) REFERENCES Task_Type (TypeID)
 -- );
 
--- "Assigned_Tasks(UserID: string, TaskID: number, Name: String, Duedate: Date, Notes: String, 
--- Description: String, TypeID: number, Assigned to: string, Assigned Date/Time: Date
+-- "Assigned_Tasks(UserID: string, TaskID: integer, Name: String, Duedate: Date, Notes: String, 
+-- Description: String, TypeID: integer, Assigned to: string, Assigned Date/Time: Date
 -- Primary Key: (UserID, TaskID)
 -- No Candidate Key
 -- Foriegn Key: TaskID, UserID, Assigned to"
 --CREATE TABLE Assigned_Tasks (
 ---    UserID VARCHAR(20) NOT NULL,
---    TaskID NUMBER PRIMARY KEY,
+--    TaskID integer PRIMARY KEY,
 --    Task_Name VARCHAR(35),
 --    Duedate TIMESTAMP,
 --    Notes VARCHAR(250),
  --   Description VARCHAR(100),
- --   TypeID NUMBER,
+ --   TypeID integer,
  --   Assigned_To VARCHAR(20),
- --   Assign_Date_Time TIMESTAMP DEFAULT SYSDATE,
+ --   Assign_Date_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  --   FOREIGN KEY (UserID) REFERENCES USERS (UserID),
  --   FOREIGN KEY (TypeID) REFERENCES Task_Type (TypeID),
     -- FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID),
@@ -137,21 +137,21 @@ CREATE SEQUENCE Task_seq
 --);
 
 
--- "Completed_Tasks(UserID: string, TaskID: number, Name: String, Duedate: Date, Notes: String, 
--- Description: String, TypeID: number, Completed Date/Time: Date
+-- "Completed_Tasks(UserID: string, TaskID: integer, Name: String, Duedate: Date, Notes: String, 
+-- Description: String, TypeID: integer, Completed Date/Time: Date
 -- Primary Key: (UserID, TaskID)
 -- No Candidate Key
 -- Foriegn Key: TaskID, UserID"
 --CREATE TABLE Completed_Tasks (
   --  UserID VARCHAR(20) NOT NULL,
-    --TaskID NUMBER PRIMARY KEY,
+    --TaskID integer PRIMARY KEY,
     --Task_Name VARCHAR(35),
     --Duedate TIMESTAMP,
     --Notes VARCHAR(250),
     --Description VARCHAR(100),
-    --TypeID NUMBER,
-    --Completed_Date_Time TIMESTAMP DEFAULT SYSDATE,
-    --ParentTaskID Number, 
+    --TypeID integer,
+    --Completed_Date_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    --ParentTaskID integer, 
     --FOREIGN KEY (UserID) REFERENCES USERS (UserID),
     --FOREIGN KEY (TypeID) REFERENCES Task_Type (TypeID)
     --Foreign Key (ParentTaskID) Re
@@ -162,15 +162,15 @@ CREATE SEQUENCE Task_seq
 CREATE TABLE Tasks (
     Assigned_by_UserID VARCHAR(20) NOT NULL,
     Assigned_to_UserID VARCHAR(20) NOT NULL,
-    TaskID NUMBER PRIMARY KEY,
+    TaskID integer PRIMARY KEY,
     Task_Name VARCHAR(35),
     Duedate TIMESTAMP,
     Notes VARCHAR(250),
     Description VARCHAR(100),
-    TypeID NUMBER,
-    Assigned_Date_Time TIMESTAMP DEFAULT SYSDATE,
-    Completed_Date_Time TIMESTAMP DEFAULT SYSDATE,
-    ParentTaskID Number, 
+    TypeID integer,
+    Assigned_Date_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Completed_Date_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ParentTaskID integer, 
     FOREIGN KEY (Assigned_by_UserID) REFERENCES USERS (UserID),
     FOREIGN KEY (Assigned_to_UserID) REFERENCES USERS (UserID),
     FOREIGN KEY (TypeID) REFERENCES Task_Type (TypeID),
@@ -180,7 +180,7 @@ CREATE TABLE Tasks (
 
 
 
--- "Company(CompanyID: number, Name: String, Street Address: String, City: String, State: String, 
+-- "Company(CompanyID: integer, Name: String, Street Address: String, City: String, State: String, 
 -- ZipCode: String, Description: String)
 -- Primary Key: CompanyID
 -- No Candidate Key
@@ -189,10 +189,10 @@ CREATE SEQUENCE Company_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Company (
-    CompanyID Number PRIMARY KEY,
+    CompanyID integer PRIMARY KEY,
     Comapny_Name VARCHAR(30),
     Street_Address VARCHAR(50),
     City VARCHAR(30),
@@ -202,8 +202,8 @@ CREATE TABLE Company (
 );
 
 
--- "Business_Contacts(ContactID: number, First_Name: String, Last_name: String, Phone: String, 
--- Email: String, Job Title: String, CompanuID: number
+-- "Business_Contacts(ContactID: integer, First_Name: String, Last_name: String, Phone: String, 
+-- Email: String, Job Title: String, CompanuID: integer
 -- Primary Key: ContactID
 -- Candidate Key: email
 -- Foreign Key: CompanyID"
@@ -211,33 +211,33 @@ CREATE SEQUENCE Business_Contacts_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Business_Contacts (
-    ContactID NUMBER PRIMARY KEY,
+    ContactID integer PRIMARY KEY,
     First_Name VARCHAR(30),
     Last_Name VARCHAR(30),
     Phone CHAR(10),
     Email VARCHAR(50) UNIQUE,
     Job_Title VARCHAR(30),
-    CompanyID NUMBER,
+    CompanyID integer,
     FOREIGN KEY (CompanyID) REFERENCES Company (CompanyID)
 );
 
 
--- "Is Contacted(TaskID: number, ContactID: number)
+-- "Is Contacted(TaskID: integer, ContactID: integer)
 -- Primary Key: (TaksID, ContactID)
 -- No Candidate Key
 -- Foriegn Key: TaskID, ContactID"
 CREATE TABLE Is_Contacted (
-    TaskID NUMBER,
-    ContactID NUMBER,
+    TaskID integer,
+    ContactID integer,
     PRIMARY KEY (TaskID, ContactID),
     FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID),  
     FOREIGN KEY (ContactID) REFERENCES Business_Contacts (ContactID)
 );
 
--- "Feature_Inventory(ItemID: number, Name: String, Quantity: number)
+-- "Feature_Inventory(ItemID: integer, Name: String, Quantity: integer)
 -- Primary Key: ItemID
 -- No Candidate Key
 -- No Foriegn Key"
@@ -245,15 +245,15 @@ CREATE SEQUENCE Feature_Inventory_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Feature_Inventory (
-    ItemID NUMBER Primary KEY,
+    ItemID integer Primary KEY,
     Item_Name VARCHAR(50),
     Quantity INTEGER
 );
 
--- "Event Features(FeatureID: number, Name: String, Description: String, Count: interger, ItemID: Number)
+-- "Event Features(FeatureID: integer, Name: String, Description: String, Count: interger, ItemID: integer)
 -- Primary Key: FeatureID
 -- No Candidate Key
 -- Forieng Key: ItemID"
@@ -261,24 +261,24 @@ CREATE SEQUENCE Event_Feature_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 Create TABLE Event_Features (
-    FeatureID NUMBER Primary KEY,
+    FeatureID integer Primary KEY,
     Feature_Name VARCHAR(30),
     Descritpion VARCHAR(100),
     Feature_Count INTEGER,
-    ItemID NUMBER,
+    ItemID integer,
     FOREIGN KEY (ItemID) REFERENCES Feature_Inventory (ItemID)
 );
 
--- "Satisfies(TaskID: number, FeatureID: number)
+-- "Satisfies(TaskID: integer, FeatureID: integer)
 -- Primary Key: (TaskID, FeatureID)
 -- No Candidate Key
 -- Foriegn Key: TaskID, FeatureID"
 CREATE TABLE Satisfies (
-    TaskID NUMBER,
-    FeatureID NUMBER,
+    TaskID integer,
+    FeatureID integer,
     PRIMARY KEY (TaskID, FeatureID),
     FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID),
     FOREIGN KEY (FeatureID) REFERENCES Event_Features (FeatureID)
@@ -286,7 +286,7 @@ CREATE TABLE Satisfies (
 
 
 
--- "Event Types(Event TypeID: number, Name: String)
+-- "Event Types(Event TypeID: integer, Name: String)
 -- Primary Key: Event TypeID
 -- No Candidate Key
 -- No Foriegn Key"
@@ -294,15 +294,15 @@ CREATE SEQUENCE Event_Type_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+   ;
 
 CREATE TABLE Event_Types (
-    Event_TypeID NUMBER PRIMARY KEY,
+    Event_TypeID integer PRIMARY KEY,
     Event_Type_Name VARCHAR(50)
 );
 
 
--- "Location Type(Location Type ID: number, Name: String)
+-- "Location Type(Location Type ID: integer, Name: String)
 -- Primary Key: Location TypeID
 -- No Candidate Key
 -- No Foriegn Key"
@@ -310,16 +310,16 @@ CREATE SEQUENCE Location_Type_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Location_types (
-    Location_typeID NUMBER Primary KEY,
+    Location_typeID integer Primary KEY,
     Loc_Type_Name VARCHAR(50)
 );
 
 
 
--- "Locations(LocID: number, Name: String, Descritpion: String, Capacity: integer, Location TypeID: number
+-- "Locations(LocID: integer, Name: String, Descritpion: String, Capacity: integer, Location TypeID: integer
 -- Primary Key: LocID
 -- No Candidate Key
 -- Foriegn Key: Location TypeID"
@@ -327,21 +327,21 @@ CREATE SEQUENCE Location_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Locations (
-    LocID NUMBER PRIMARY KEY,
+    LocID integer PRIMARY KEY,
     Location_Name VARCHAR(50),
     DESCRIPTION Varchar(100),
     Capacity INTEGER,
-    Location_typeID NUMBER,
+    Location_typeID integer,
     FOREIGN KEY (Location_typeID) REFERENCES Location_types (Location_typeID)
 );
 
 
 
--- "Events(EventID: number, Name: String, Number of Attendees: integer, Start Date/Time: timestamp, 
--- end Date/Time: timestamp, Event TypeID: number, LocID: number, Description: String
+-- "Events(EventID: integer, Name: String, integer of Attendees: integer, Start Date/Time: timestamp, 
+-- end Date/Time: timestamp, Event TypeID: integer, LocID: integer, Description: String
 -- Primary Key: EventID
 -- No Candidate Key
 -- Foriegn Key: Event Type ID, LocID"
@@ -349,81 +349,81 @@ CREATE SEQUENCE Event_seq
     MINVALUE 1
     START WITH 1
     INCREMENT BY 1
-    NOCACHE;
+    ;
 
 CREATE TABLE Events (
-    EventID Number PRIMARY KEY,
+    EventID integer PRIMARY KEY,
     Event_Name VARCHAR(50),
     Attendees_conut INTEGER,
     Start_Time_Date TIMESTAMP,
     End_Time_Date TIMESTAMP,
-    Event_TypeID NUMBER,
-    LocID NUMBER,
+    Event_TypeID integer,
+    LocID integer,
     DESCRIPTION VARCHAR(100),
     FOREIGN KEY (Event_TypeID) REFERENCES Event_Types (Event_TypeID),
     FOREIGN KEY (LocID) REFERENCES Locations (LocID)
 );
 
--- "Associated_With(EventID: number, FeatureID: number)
+-- "Associated_With(EventID: integer, FeatureID: integer)
 -- Primary key: (EventID, FeatureID)
 -- No Candidate Key
 -- Foriegn Key: EventID, Feature ID"
 CREATE TABLE Associated_With (
-    EventID NUMBER,
-    FeatureID NUMBER,
+    EventID integer,
+    FeatureID integer,
     PRIMARY KEY (EventID, FeatureID),
     FOREIGN KEY (EventID) REFERENCES Events (EventID),
     FOREIGN KEY (FeatureID) REFERENCES Event_Features (FeatureID)
 );
 
 
--- "Administrates(UserID: String, EventID: number)
+-- "Administrates(UserID: String, EventID: integer)
 -- Primary Key: (UserID, EventID)
 -- No Candidate Key
 -- Foriegn Key: UserID, EventID"
 CREATE TABLE Administrates (
     UserID VARCHAR(20),
-    EventId NUMBER,
+    EventId integer,
     PRIMARY Key (UserID, EventID),
     FOREIGN KEY (EventID) REFERENCES Events (EventID),
     FOREIGN KEY (UserID) REFERENCES Admins (UserID)
 );
 
--- "Hosts(UserID: String, EventID: number)
+-- "Hosts(UserID: String, EventID: integer)
 -- Primary Key: (UserID, EventID)
 -- No Candidate Key
 -- Foriegn Key: UserID, EventID"
 CREATE TABLE Hosts (
     UserID VARCHAR(20),
-    EventId NUMBER,
+    EventId integer,
     PRIMARY Key (UserID, EventID),
     FOREIGN KEY (EventID) REFERENCES Events (EventID),
     FOREIGN KEY (UserID) REFERENCES Event_Hosts (UserID)
 );
 
--- "Is Present at(UserID: String, EventID: number, sign in Date/time: timestamp, sign out Data/Time: timestamp)
+-- "Is Present at(UserID: String, EventID: integer, sign in Date/time: timestamp, sign out Data/Time: timestamp)
 -- Primary Key: (UserID, EventID)
 -- No Candidate Key
 -- Foriegn Key: UserID, EventID"
 CREATE TABLE Is_Present_at (
     UserID VARCHAR(20),
-    EventID NUMBER,
-    Sign_In TIMESTAMP DEFAULT SYSDATE,
-    Sign_Out TIMESTAMP DEFAULT SYSDATE,
+    EventID integer,
+    Sign_In TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Sign_Out TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY Key (UserID, EventID),
     FOREIGN KEY (EventID) REFERENCES Events (EventID),
     FOREIGN KEY (UserID) REFERENCES Event_Hosts (UserID)
 );
 
 CREATE TABLE Vendor (
-    CompanyID NUMBER, 
+    CompanyID integer, 
     Equipment_type VARCHAR(50), 
     PRIMARY Key (CompanyID),
     FOREIGN KEY (CompanyID) REFERENCES Company (CompanyID)
 );
 
 CREATE TABLE Caterer (
-    CompanyID NUMBER, 
+    CompanyID integer, 
     Cuisine_type VARCHAR(30), 
     PRIMARY Key (CompanyID),
     FOREIGN KEY (CompanyID) REFERENCES Company (CompanyID)
