876a964f308858007a017576d2440e20d410064b
aalapsley <51500063+aalapsley@users.noreply.github.com>
2024-04-13 14:33:13 -0400

    Added navbar to existing main, see readme in navbar folder to run changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,37 @@ import AddEvent from "./components/AddEvent";
 import AddHostandAdmin from "./components/AddHostandAdmin";
 import EventSummary from "./pages/EventsSummary";
 
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import EventFeatureGallery from "./pages/eventfeaturegallery";
+
+import EventRequest from "./pages/eventrequest";
+ 
+function App () {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/eventrequest"
+                    element={<EventRequest />}
+                />
+                <Route path="/eventfeaturegallery" element={<EventFeatureGallery />} />
+            </Routes>
+        </Router>
+    );
+}
+ 
+
+/*
 function App() {
   return (
     <Fragment>
@@ -19,4 +50,10 @@ function App() {
   );
 }
 
+                <Route
+                    path="/sign-out"
+                    element={<SignOut />}
+                />
+
+*/
 export default App;
---
+++ client/src/pages/eventfeaturegallery.js
@@ -0,0 +1,7 @@
+import React from "react";
+ 
+const EventFeatureGallery = () => {
+    return <h1>Event Features! Find a way to hide this from non-admins later</h1>;
+};
+ 
+export default EventFeatureGallery;
